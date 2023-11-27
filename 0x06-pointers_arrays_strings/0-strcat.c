b43ac63c34818d029fe576d8735ca6588b14d9f2
sabzerro <sabelontsie@gmail.com>
2023-11-27 13:47:58 -0800

    main.h, README.md, 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatinates 2 strings.
+ * @dest: First string
+ * @src: Second string
+ *
+ * Return: String
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[j];
+		a++;
+		b++;
+	}
+
+	dest[a] = '\0';
+
+	return dest;
+}
